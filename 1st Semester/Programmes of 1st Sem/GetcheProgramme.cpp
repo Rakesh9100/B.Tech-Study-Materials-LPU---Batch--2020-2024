4c417386b1ee7f4337cbd992d9da7c66a0e4096a
Rakesh Roshan <roshanrakesh7362@gmail.com>
2023-01-19 23:58:37 +0530

    1st Sem Study Materials Added

---
+++ 1st Semester/Programmes of 1st Sem/GetcheProgramme.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char s;
+char name[20];//string
+gets(name);
+puts(name);
+s=getche();
+printf("The entered cahracter is %c",putch(s));
+s=getchar();
+printf(" The entered character is %c",putchar(s));
+getch();
+}
---
+++ 1st Semester/Programmes of 1st Sem/GoodNight.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+		while(1!=0)
+		
+		cout<<"Good		night";
+}
+
